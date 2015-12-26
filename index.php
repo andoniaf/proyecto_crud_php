6a518c03f5a860d9b34c4f0f042141cc6505fa3c
andoniaf <andonialonsof@gmail.com>
2015-12-26 18:31:44 +0100

    Modificar index.php y Añadir css/ y img/

--- index.php
+++ index.php
@@ -2,86 +2,44 @@
     <head>
         <meta charset="UTF-8">
         <title>Proyecto CRUD</title>
+	    <link media="all" href="css/style.css" rel="stylesheet" type="text/css"></link>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
         <script type="text/JavaScript">
             function borra_cliente(id){
-		var answer = confirm('¿Estás seguro que deseas borrar el cliente?');
-		if (answer) {
-		// si el usuario hace click en ok, 
-		// se ejecutar borra.php
-		window.location = 'borra.php?id=' + id;
-		}
+        		var answer = confirm('¿Estás seguro que deseas borrar el cliente?');
+        		if (answer) {
+        		// si el usuario hace click en ok,
+        		// se ejecutar borra.php
+        		window.location = 'borra.php?id=' + id;
+        		}
             }
-        </script> 
+        </script>
     </head>
     <body>
-	<h1>Listado de Clientes</h1>
+    <div id="wrapper">
+	<div id="header">
+	    <div id="logo">
+		<img src="img/logo_web.jpeg"></img>
+	    </div>
+	    <div id="title">
+		Manager Emulator LOL
+	    </div>
+
+	</div>
+    <nav class="navbar navbar-default">
+        <!-- http://getbootstrap.com/customize/?id=3f3323b47a99e302d879#navbar -->
+    </nav>
+	<div id="content">
 	<?php
-	// incluir la conexión a la base de datos
-	include 'conexion.php';
 
-	$action = isset($_GET['action']) ? $_GET['action'] : "";
-	// si viene de borra.php
-        if ($action == 'deleted') {
-            echo "<div>El registro cliente ha sido borrado.</div><br>";
-        }
-	
-	// Elegir los datos que deseamos recuperar de la tabla
-	$query = "SELECT id,nif,nombre,apellido1,apellido2,email,telefono,usuario "
-		. "FROM clientes "
-		. "ORDER BY apellido1, apellido2, nombre";
-
-	// Preparamos y ejecutamos la consulta
-	if ($stmt = $conexion->prepare($query)) {
-	if (!$stmt->execute()) {
-	    die('Error de ejecución de la consulta. ' . $conexion->error);
-	} 
-
-	// recogemos los datos
-	$stmt->bind_result($id,$nif,$nombre,$apellido1,$apellido2,$email,$telefono,
-		$usuario);
-
-	// enlace a alta
-	echo "<div>";
-	echo "<a href='alta.php'>Alta cliente</a>";
-	echo "</div>";
-
-	//cabecera de los datos mostrados
-	echo "<table>"; //start table
-	//creating our table heading
-	echo "<tr>";
-	echo "<th>NIF</th>";
-	echo "<th>Nombre</th>";
-	echo "<th>Apellido 1</th>";
-	echo "<th>Apellido 2</th>";
-	echo "<th>email</th>";
-	echo "<th>telefono</th>";
-	echo "<th>usuario</th>";
-	echo "</tr>";
-	//recorrido por el resultado de la consulta
-	while ($stmt->fetch()) {
-	echo "<tr>";
-	echo "<td>$nif</td>";
-	echo "<td>$nombre</td>";
-	echo "<td>$apellido1</td>";
-	echo "<td>$apellido2</td>";
-	echo "<td>$email</td>";
-	echo "<td>$telefono</td>";
-	echo "<td>$usuario</td>";
-	echo "<td>";
-	// Este enlace es para modificar el registro
-	echo "<a href='edita.php?id={$id}'>Edita</a>";
-	echo " / ";
-	// Este enlace es para borrar el registro y también se explicará más tarde
-	echo "<a href='javascript:borra_cliente(\"$id\")'> Elimina </a>";
-	echo "</td>";
-	echo "</tr>\n";
-	}
-	// end table
-	echo "</table>";
-	$stmt->close();
-	} else {
-	die('Imposible preparar la consulta. ' . $conexion->error);
-	}
 	?>
+	</div>
+	<div id="footer">
+        <div class="pull-left"><kbd>#MELOL</kbd></div>
+	    <div class="pull-right"><kbd>Andoni Alonso (2015)</kbd></div>
+	</div>
+    </div>
     </body>
 </html>
